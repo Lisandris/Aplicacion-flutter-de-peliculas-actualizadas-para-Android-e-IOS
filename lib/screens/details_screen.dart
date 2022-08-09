916517c1864e94ee3e3550b32eebe607ad68e5ab
Lisandris <lis170694@gmail.com>
2022-08-09 22:01:27 +0200

    Poster y diseño principal de la app

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -12,7 +14,13 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
          slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+              
+            ])
+          )
         ],
       )
     );
@@ -44,13 +52,56 @@ class   _CustomAppBar extends StatelessWidget {
             ),
         ),
         
-    
 
           background: const FadeInImage(
             placeholder: AssetImage('assets/loading.gif'), 
             image: NetworkImage('https://curiosfera-animales.com/wp-content/uploads/2016/09/El-car%C3%A1cter-del-gato-domestico.jpg'),
             fit: BoxFit.cover,
           ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+    
+    return Container(
+      margin: EdgeInsets.only( top:20),
+      padding: EdgeInsets.symmetric( horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+               image: NetworkImage( 'https://via.placeholder.com/200x300'),
+               height: 150,
+            ),
+          ),
+
+          SizedBox( width: 20),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Text( 'movie.tilte' , style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2),
+              Text( 'movie.originalTitle', style: textTheme.subtitle1, overflow: TextOverflow.ellipsis),
+
+              Row(
+                children: [
+                  Icon( Icons.star_border_outlined, size: 20, color: Colors.amber),
+                  SizedBox( width: 5),
+                  Text('movie.vote.voteAvergage', style: textTheme.caption )
+                ],
+              )
+            ]
+          )
+        ]
       ),
     );
   }
